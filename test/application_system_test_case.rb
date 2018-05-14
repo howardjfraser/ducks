02b3232e0c638f6356132569ffb31ec81cb76fad
Howard Fraser <howardjfraser@gmail.com>
2018-05-14 21:21:28 +1000

    Add command_name for system tests to enable merged coverage.

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,7 @@
 require 'test_helper'
 
+SimpleCov.command_name 'test:system'
+
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
 end
